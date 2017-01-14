4ee347b6e7b6eb397e11aae0ca40a62d4e1dab6a
Yuhui Li <a@yuhuili.com>
2017-01-14 11:53:45 -0500

    Complete rank report

--- Server/TouchBarDinoXServer.cpp
+++ Server/TouchBarDinoXServer.cpp
@@ -41,7 +41,7 @@ std::string make_daytime_string() {
 
 class database_manager {
 public:
-  bool write(std::string& player_id, std::string& player_name, double player_score, std::string& player_ip, std::string& player_system, std::string& timestamp) {
+  bool write(std::string& player_id, std::string& player_name, double player_score, std::string& player_ip, std::string& player_system, std::string& timestamp, int *r) {
     rc = sqlite3_open("dino-test.db", &db);
     if (rc) {
       return false;
@@ -49,16 +49,40 @@ public:
     
     query = "INSERT INTO scoreboard (player_id, player_name, player_score, player_ip, player_system, timestamp) VALUES ('"+player_id+"','"+player_name+"',"+std::to_string(player_score)+",'"+player_ip+"','"+player_system+"','"+timestamp+"');";
     
-    std::cout << query << std::endl;
-    
     const char *cquery = query.c_str();
     
     rc = sqlite3_exec(db, cquery, 0, 0, &zErrMsg);
     if (rc!=SQLITE_OK){
       std::cout << "Database error" << std::endl;
       sqlite3_free(zErrMsg);
+      return false;
     }
     
+    lastInsertRow = sqlite3_last_insert_rowid(db);
+    
+    
+    query = "SELECT \
+        (\
+          SELECT  COUNT(*)\
+          FROM    scoreboard AS s2\
+          WHERE   s2.player_score > s1.player_score\
+        )\
+        FROM    scoreboard AS s1\
+        WHERE   s1.ROWID = "+std::to_string(lastInsertRow)+"";
+        
+    const char *cquery2 = query.c_str();
+    int rank = 0;
+    
+    rc = sqlite3_exec(db, cquery2, insertCallback, &rank, &zErrMsg);
+    if (rc!=SQLITE_OK){
+      std::cout << "Database error" << std::endl;
+      sqlite3_free(zErrMsg);
+      return false;
+    }
+    
+    *r = rank;
+    
+    std::cout << rank << std::endl;
     sqlite3_close(db);
     return true;
   }
@@ -68,6 +92,13 @@ private:
   char *zErrMsg = 0;
   int rc;
   std::string query;
+  int lastInsertRow = 0;
+  
+  static int insertCallback(void *rank, int argc, char **argv, char **azColName){
+    int *c = (int*) rank;
+    *c = atoi(argv[0]);
+    return 0;
+  }
 };
 
 class tcp_connection
@@ -194,7 +225,18 @@ public:
       ss << seconds;
       timestamp = ss.str();
       
-      dbm->write(player_id, player_name, player_score, player_ip, player_system, timestamp);
+      int rank = -1;
+      
+      if (dbm->write(player_id, player_name, player_score, player_ip, player_system, timestamp, &rank)) {
+        message_ = "{\"status\":0, \"rank\":"+std::to_string(rank)+"}\n";
+      } else {
+        message_ = "{\"status\":1}\n";
+      }
+      
+      boost::asio::async_write(socket_, boost::asio::buffer(message_),
+        boost::bind(&tcp_connection::handle_write, shared_from_this(),
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
       
     }
   }
